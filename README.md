b3cb686c89386b04dc4e17d782cdfa9af7b188f9
Elizabeth Nenniger <elidlocke@gmail.com>
2017-12-26 21:52:22 -0500

    adding git tracking

---
+++ README.md
@@ -0,0 +1,8 @@
+# Wedding Website
+A static website made for the Wedding of Ben & Beth. Based loosely off of the agency bootstrap theme.
+
+## Preview
+[benandbethwedding.com](benandbethwedding.com)
+
+## Props
+Thank you to [this article](https://blog.webjeda.com/google-form-customize/) for the google spreadsheets form submission hack! This made going through the RSVPs a google sheets breeze.
---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+var submitted = false;
+
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Hide navbar when modals trigger
+  $('.portfolio-modal').on('show.bs.modal', function(e) {
+    $(".navbar").addClass("d-none");
+  })
+  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+    $(".navbar").removeClass("d-none");
+  })
+
+  //Show and Hide Form Fields
+  $('input[id=accept]').change(function(e) {
+    $(".decline-form").addClass("hide");
+    $(".accept-form").removeClass("hide");
+  });
+  //Show and Hide Form Fields
+  $('input[id=decline]').change(function(e) {
+    $(".decline-form").removeClass("hide");
+    $(".accept-form").addClass("hide");
+  });
+  //hide fields on form submit
+  $( "#rsvp-submit" ).click(function(e) {
+    $(".rsvp-form").addClass("hide");
+    $(".thanks").removeClass("hide");
+  });
+})(jQuery); // End of use strict
+
+
